626f33af212b8caa11a02fa373bd469123a6f269
a.espinel <a.espinel@opendeusto.es>
2026-04-04 21:13:34 +0200

    Usuarios

--- usuario.h
+++ usuario.h
@@ -10,19 +10,19 @@ typedef struct {
     char nombre[100];
 } Usuario;
 
-/* Crea la tabla usuarios si no existe */
+// Crea la tabla usuarios si no existe
 int  usuario_crear_tabla(sqlite3* db);
 
-/* Registra un nuevo usuario. Devuelve 1 OK, 0 si ya existe o error */
+// Registra un nuevo usuario. Devuelve 1 OK, 0 si ya existe o error 
 int  usuario_registrar(sqlite3* db, const char* username,
                        const char* password, const char* nombre);
 
-/* Login: rellena 'out' si las credenciales son correctas.
- * Devuelve 1 si OK, 0 si no */
+// Login: rellena 'out' si las credenciales son correctas.
+// Devuelve 1 si OK, 0 si no 
 int  usuario_login(sqlite3* db, const char* username,
                    const char* password, Usuario* out);
 
-/* Comprueba si ya existe un username. Devuelve 1 si existe */
+// Comprueba si ya existe un username. Devuelve 1 si existe 
 int  usuario_existe(sqlite3* db, const char* username);
 
 #endif
